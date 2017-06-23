fb4ccc849b87654974b6eb00008397e1e4971f20
alejandrochvs <alejandrochvs@hotmail.com>
2017-06-23 10:56:29 -0500

    Register verification

--- public/js/main.js
+++ public/js/main.js
@@ -24,10 +24,10 @@ $(function () {
         current,
         next,
         loadAdmin,
-        previous,
-        //        admin,
-        //Functions
-        requestStringChange = function (toFind, toChange) {
+        previous;
+    //        admin,
+    //Functions
+    var requestStringChange = function (toFind, toChange) {
             toChange = "'" + toChange + "'";
             toFind = "var " + toFind;
             var data = {
@@ -645,8 +645,24 @@ $(function () {
                     $('.head.title > .cont').html(headTitle4);
                     $('.head.title > .cont').attr('data-var', 'headTitle4');
                     $('body > .header').removeClass('boy girl nBoy nGirl bBoy bGirl');
-                    var register = function (username, email, password, name, last_name, gender, birthday, access_level) {
-                        if (username !== "" && password !== "" && name !== "" && email !== "" && last_name !== "" && gender !== "" && birthday !== "" && access_level !== "") {
+                    var register = function (username, email, password,passwordVer, name, last_name, gender, birthday, access_level) {
+                        function validateEmail(emailToValidate) {
+                            var re = /\S+@\S+\.\S+/;
+                            return re.test(emailToValidate);
+                        };
+                        function validateUsername(usernameToValidate) {
+                            var re = /^[a-zA-Z\-]+$/;
+                            return re.test(usernameToValidate);
+                        };
+                        function validatePassword(passwordToValidate) {
+                            var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+                            if (password == passwordVer && re.test(passwordToValidate)){
+                                return true;
+                            }else{
+                                return false;
+                            }
+                        };
+                        if (validateUsername && validatePassword && validateUsername(name) && validateEmail(email) && validateUsername(name) && gender !== "" && birthday !== "" && access_level !== "") {
                             var data = {
                                 username: username,
                                 mail: email,
@@ -672,6 +688,38 @@ $(function () {
                                 }
                             });
                         } else {
+                            if (!validateEmail(email)) {
+                                $('#email').css('border-bottom','solid 2px red');
+                                console.log('Wrong e-mail.')
+                            }else{
+                                $('#email').css('border-bottom','solid 2px transparent');
+                            }
+                            if (!validateUsername(username)) {
+                                $('#username').css('border-bottom','solid 2px red');
+                                console.log('Wrong username.')
+                            }else{
+                                $('#username').css('border-bottom','solid 2px transparent');
+                            }
+                            if (!validatePassword(password)) {
+                                $('#password').css('border-bottom','solid 2px red');
+                                $('#password-ver').css('border-bottom','solid 2px red');
+                                console.log('Wrong password.')
+                            }else{
+                                $('#password').css('border-bottom','solid 2px transparent');
+                                $('#password-ver').css('border-bottom','solid 2px transparent');
+                            }
+                            if (!validateUsername(name)) {
+                                $('#name').css('border-bottom','solid 2px red');
+                                console.log('Wrong name.')
+                            }else{
+                                $('#name').css('border-bottom','solid 2px transparent');
+                            }
+                            if (!validateUsername(last_name)) {
+                                $('#last_name').css('border-bottom','solid 2px red');
+                                console.log('Wrong last name.')
+                            }else{
+                                $('#last_name').css('border-bottom','solid 2px transparent');
+                            }
                             console.log('Fill everything.');
                         }
                     };
@@ -679,12 +727,13 @@ $(function () {
                         var username = $('#username').val();
                         var email = $('#email').val();
                         var password = $('#password').val();
+                        var passwordVer = $('#password-ver').val();
                         var name = $('#name').val();
                         var last_name = $('#last_name').val();
                         var gender = $('#gender').val();
                         var birthday = $('#birthday').val();
                         var access_level = $('#access_level').val();
-                        register(username, email, password, name, last_name, gender, birthday, access_level);
+                        register(username, email, password,passwordVer, name, last_name, gender, birthday, access_level);
                     });
                     $('.register > input').keyup(function (e) {
                         if (e.keyCode === 13) {
@@ -749,7 +798,7 @@ $(function () {
                     console.log(res);
                 } else {
                     access_level = res.access_level;
-                    $('body').prepend('<div class="ui col-xs-12"> <input type="color" class="colorPicker" style="display:none;"> <div class="ui-section col-xs-1"> <div class="ui-option backAdmin col-xs-6"><i class="fa fa-angle-left" aria-hidden="true"></i></div> <div class="ui-option nextAdmin col-xs-6"><i class="fa fa-angle-right" aria-hidden="true"></i></div> </div> <div class="ui-section col-xs-1"> <div class="ui-option toggle-edit-mode col-xs-12"> <h6>Edit mode</h6> </div> </div> <div class="col-xs-2 ui-section"> <div class="col-xs-12 ui-option admin-color"> <h6>Color</h6> </div> <div class="col-xs-12 ui-hidden color-hidden"> <ul class="colors"> <li> <h5>Boy</h5> <div data-var="boy" class="color"> <h5>Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Girl</h5> <div data-var="girl" class="color"> <h5>Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Baby Boy</h5> <div data-var="bBoy" class="color"> <h5>Baby Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Baby Girl</h5> <div data-var="bGirl" class="color"> <h5>Baby Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Newborn Boy</h5> <div data-var="nBoy" class="color"> <h5>Newborn Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Newborn Girl</h5> <div data-var="nGirl" class="color"> <h5>Newborn Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> </ul> </div> </div> <div class="col-xs-4 ui-section"> <div class="col-xs-12 ui-option" style="cursor:default;padding:0">Admin mode</div> </div> <div class="col-xs-1 ui-section"> <div class="col-xs-12 ui-option admin-sections"> <h6>Sections</h6> </div> <div class="col-xs-12 ui-hidden sections-hidden"> <ul class="section-divs"> </ul> </div> </div> <div class="col-xs-2 ui-section user"> <div class="ui-option col-xs-12 admin-user" style="padding:0 3px;"> <h6 class="col-xs-8" style="padding-right: 0;">Username</h6><i class="fa fa-user-o col-xs-4" aria-hidden="true" style="padding:0;"></i></div> <div class="col-xs-12 ui-hidden user-hidden"> <ul class="user-settings"> </li> <li class="log-out"> <h5>Log out</h5> </li> </ul> </div> </div> <div class="col-xs-1 ui-section hide-admin"> <div class="col-xs-12 ui-option"><i class="fa fa-times fa-1x exit" aria-hidden="true"></i></div> </div> </div>');
+                    $('body').prepend('<div class="ui col-xs-12"> <input type="color" class="colorPicker" style="display:none;"> <div class="ui-section col-xs-1"> <div class="ui-option backAdmin col-xs-6"><i class="fa fa-angle-left" aria-hidden="true"></i></div> <div class="ui-option nextAdmin col-xs-6"><i class="fa fa-angle-right" aria-hidden="true"></i></div> </div> <div class="ui-section col-xs-1"> <div class="ui-option toggle-edit-mode col-xs-12"> <h6>Edit mode</h6> </div> </div> <div class="col-xs-2 ui-section"> <div class="col-xs-12 ui-option admin-color"> <h6>Color</h6> </div> <div class="col-xs-12 ui-hidden color-hidden"> <ul class="colors"> <li> <h5>Boy</h5> <div data-var="boy" class="color"> <h5>Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Girl</h5> <div data-var="girl" class="color"> <h5>Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Baby Boy</h5> <div data-var="bBoy" class="color"> <h5>Baby Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Baby Girl</h5> <div data-var="bGirl" class="color"> <h5>Baby Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Newborn Boy</h5> <div data-var="nBoy" class="color"> <h5>Newborn Boy</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> <li> <h5>Newborn Girl</h5> <div data-var="nGirl" class="color"> <h5>Newborn Girl</h5> </div> <div class="color-edit"><i class="fa fa-pencil" aria-hidden="true"></i></div> </li> </ul> </div> </div> <div class="col-xs-4 ui-section"> <div class="col-xs-12 ui-option" style="cursor:default;padding:0">Admin mode</div> </div> <div class="col-xs-1 ui-section"> <div class="col-xs-12 ui-option admin-sections"> <h6>Sections</h6> </div> <div class="col-xs-12 ui-hidden sections-hidden"> <ul class="section-divs"> </ul> </div> </div> <div class="col-xs-2 ui-section user"> <div class="ui-option col-xs-12 admin-user" style="padding:0 3px;"> <h6 class="col-xs-8" style="padding-right: 0;">Username</h6><i class="fa fa-user-o col-xs-4" aria-hidden="true" style="padding:0;"></i></div> <div class="col-xs-12 ui-hidden user-hidden"> <ul class="user-settings"> </li> <li class="settings"> <h5>Settings</h5> </li> <li class="log-out"> <h5>Log out</h5> </li> </ul> </div> </div> <div class="col-xs-1 ui-section hide-admin"> <div class="col-xs-12 ui-option"><i class="fa fa-times fa-1x exit" aria-hidden="true"></i></div> </div> </div>');
                     if (access_level === '8') {
                         $('.user-settings').prepend('<li class="register"> <h5>Register user</h5> </li>');
                         $('.register').click(function () {
