269f2ef804941fed1a994b2da1c674cca3f54d63
alejandrochvs <alejandrochvs@hotmail.com>
2017-08-01 07:37:29 -0500

    Auto push V1.0.0

--- git-add.js
+++ git-add.js
@@ -1,20 +1,48 @@
 var exec = require('child_process').exec;
-exec('git status .', function (err, stdout, stderr) {
-    console.log('Err : ' + err);
-    var status = stdout.split('\n')[2];
-    console.log('STDOUT : ' + status);
-    console.log('STDERR : ' + stderr);
-    if (status == 'Untracked files:'){
+var V = 100;
+var gitStatus = function () {
+    exec('git status .', function (err, stdout, stderr) {
+        var status = stdout.split('\n')[2];
+        console.log('Err : ' + err);
+        console.log('STDOUT : ' + status);
+        console.log('STDERR : ' + stderr);
+        if (status == 'Untracked files:') {
+            console.log("Untracked");
+            gitAdd();
+        } else if (status == "Changes to be committed:") {
+            gitCommit();
+        } else if (status == '  (use "git push" to publish your local commits)') {
+            gitPush();
+        }
+        return;
+    });
+}
+var gitAdd = function () {
+    exec('git add .', function (err, stdout, stderr) {
+        console.log('Err : ' + err);
+        console.log('STDOUT : ' + stdout);
+        console.log('STDERR : ' + stderr);
         console.log('GIT ADD');
-    }else if (status == "Changes to be committed:"){
+        return;
+    });
+}
+var gitCommit = function () {
+    exec('git commit -m "Auto push V' + JSON.stringify(V).split('').join('.') + '"', function (err, stdout, stderr) {
+        console.log('Err : ' + err);
+        console.log('STDOUT : ' + stdout);
+        console.log('STDERR : ' + stderr);
         console.log('GIT COMMIT');
-    }
-    return;
-    
-});
-exec('git add .', function (err, stdout, stderr) {
-    console.log('Err : ' + err);
-    console.log('STDOUT : ' + stdout);
-    console.log('STDERR : ' + stderr);
-    return;
-});
+        V++;
+        return;
+    });
+}
+var gitPush = function () {
+    exec('git push origin master', function (err, stdout, stderr) {
+        console.log('Err : ' + err);
+        console.log('STDOUT : ' + stdout);
+        console.log('STDERR : ' + stderr);
+        console.log('GIT PUSH');
+        return;
+    });
+}
+gitStatus();
