81c8e5b57563f0fafe1bdce7ef8ea50908a4bb53
alejandrochvs <alejandrochvs@hotmail.com>
2017-09-04 10:08:09 -0500

    Auto push V4.9.5

--- develop.bat
+++ develop.bat
@@ -2,4 +2,4 @@ start cmd.exe @cmd /k "mongod --auth" &
 start cmd.exe @cmd /k "sass --watch public/scss:public/css" &
 start cmd.exe @cmd /k "node git-add.js" &
 start cmd.exe @cmd /k "npm start" &
-start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "ssh -i node.pem ubuntu@ec2-18-220-23-4.us-east-2.compute.amazonaws.com" "%~1"
+start "" "%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "ssh -i node.pem ubuntu@13.59.244.24" "%~1"
